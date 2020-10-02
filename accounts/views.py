6e190a10b290367071567f4245cd9661f05c2727
Diyor Mukhamedkhanov <d.mukhamedkhanov98@gmail.com>
2020-10-02 22:03:38 +0500

    Create multiple orders field added

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 
 from .models import *
 from .forms import OrderForm
@@ -25,6 +26,7 @@ def products(request):
     return render(request,'accounts/products.html', {'products': products} )
 
 def customer(request,pk):
+    
     customer_obj = Customer.objects.get(id=pk)
     orders = customer_obj.order_set.all()
     order_count = orders.count()
@@ -33,16 +35,20 @@ def customer(request,pk):
                 'order_count':order_count}
     return render(request,'accounts/customer.html',context)
 
-def createOrder(request):
-    form = OrderForm()
+def createOrder(request,pk):
+    OrderFormSet = inlineformset_factory(Customer,Order,fields=('product','status'),extra=10)
+    customer = Customer.objects.get(id=pk)
+    formset = OrderFormSet(queryset= cOrder.objects.none(),instance=customer)
+    # form = OrderForm(initial={'customer':customer})
     if request.method == 'POST':
         #print('Printing POST:',request.POST)
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
+        # form = OrderForm(request.POST)
+        formset = OrderFormSet(request.POST,instance=customer)
+        if formset.is_valid():
+            formset.save()
             return redirect('/')
     context = {
-        'form' : form,
+        'formset' : formset,
     }
     return render(request, 'accounts/order_form.html',context)
 
